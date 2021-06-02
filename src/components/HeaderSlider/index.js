7df9146811b5023ad8fd6b728e480b707c578d16
unknown <karankataria771801@gmail.com>
2021-06-03 04:58:33 +0530

    exact

--- src/components/HeaderSlider/index.js
+++ src/components/HeaderSlider/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel';
 import './headerSlider.css';
 import Container from 'react-bootstrap/Container';
+import {Link} from 'react-router-dom';
 import ProductCard from '../ProductCard/index';
 
 const data =[
@@ -18,46 +19,58 @@ export default class HeaderSlider extends Component {
             <Container style={{backgroundColor:'#EAEDED', height:800}} fluid>
                 <Carousel indicators={false}>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/May/M17/non-reg/1500x600_Hero-Tall_JPN._CB668432235_.jpg"
                             alt="First slide"
                         />
+                        </Link>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/HPC/GW/Household_1500x600_V2._CB670004437_.jpg"
                             alt="Second slide"
                         />
+                        </Link>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/HPC/GW/Grocery_1500x600._CB669573043_.jpg"
                             alt="Third slide"
                         />
+                        </Link>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Home/AmazonTV/Ravina/1500X600._CB668498028_.png"
                             alt="Third slide"
                         />
+                        </Link>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/Gateway/Zeitgeist/Mar20/Covid19/2021/IN_GWD_Covid19_CustomerMsg_MH_ENG_1x_v1._CB669806110_.jpg"
                             alt="Third slide"
                         />
+                        </Link>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <Link to={{pathname:"/page"}}>
                         <img
                             className="d-block w-100"
                             src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/PSS/Personal-Safety_1500x600._CB668022827_.jpg"
                             alt="Third slide"
                         />
+                        </Link>
                     </Carousel.Item>
                 </Carousel>
                 <div className="grad">
